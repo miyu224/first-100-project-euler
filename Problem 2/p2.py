42aafc457489162cab84761700d17627b6a52e72
yulw22 <ya15510@gmail.com>
2026-02-23 10:42:17 +0700

    Add solutions for Problem 2: Even Fibonacci Numbers and update README

---
+++ Problem 2/p2.py
@@ -0,0 +1,14 @@
+FOUR_MILLION = 4000000
+
+def compute():
+	ans = 0
+	x = 1
+	y = 2
+	while x <= FOUR_MILLION:
+		if x % 2 == 0:
+			ans += x
+		x, y = y, x + y
+	return str(ans)
+
+if __name__ == "__main__":
+	print(compute())
