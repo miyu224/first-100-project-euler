45dab7e85b036856d80e4fdc420b6d3d330253b4
miyunguyen <ya15510@gmail.com>
2026-02-25 17:19:44 +0700

    Implement solution for Problem 4: Largest Palindrome Product; update README and answer.txt

--- Problem 4/p4.py
+++ Problem 4/p4.py
@@ -1,6 +1,13 @@
 def compute():
-  ans = 0
-  return str(ans)
+    ans = 0
+    for a in range(999, 99, -1):
+        for b in range(a, 99, -1):
+            product = str(a * b)
+            if product == product[::-1]:
+                ans = max(ans, int(product))
+
+    return str(ans)
+
 
 if __name__ == "__main__":
-	print(compute())
+    print(compute())
