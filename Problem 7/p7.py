a56c018fcd4bace8097477c30c810d526cacd5b6
miyunguyen <ya15510@gmail.com>
2026-03-04 09:45:26 +0700

    submit problem 7: 10001st Prime

--- Problem 7/p7.py
+++ Problem 7/p7.py
@@ -1,5 +1,22 @@
+import math
+
+
+def is_prime(n) -> bool:
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
 def compute():
-    ans = 0
+    ans = 3
+    count = 2
+
+    while count < 10001:
+        ans += 2
+        if is_prime(ans):
+            count += 1
+
     return str(ans)
 
 
